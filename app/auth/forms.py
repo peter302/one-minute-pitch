9a7b301970c178a578136fcdac6d7fa0084b5446
peter kuria <you@example.com>
2020-01-13 13:06:07 -0500

    created a registration form for registering new users

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,ValidationError,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+
+
+class RegistrationForm(FlaskForm):
+    '''a form for registering a new user'''
+    email=StringField('your email address',validators=[Required(),Email()])
+    username=StringField('your username here',validators=[Required()])
+    password=PasswordField('password',validators=[Required(),EqualTo('password',message='ensure passwords match')])
+    password_confirm=PasswordField('confirm password',validators=[Required()])
+    submit=Submited('sign up')
+    
