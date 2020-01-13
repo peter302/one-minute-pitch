4bd84d57f1e7193c43e40a058e26c59c82042e00
peter kuria <you@example.com>
2020-01-13 11:50:42 -0500

    imported and instantiated bootstrap,sqlalchemy and login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,11 @@
 from flask import flask
-from flask_bootstrap import flask_bootstrap
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import flask_sqlalchemy
+from flas_login import loginManager
+
+
+
+bootstrap=Bootstrap()
+db=SQLAlchemy()
+login_manager=loginManager()
