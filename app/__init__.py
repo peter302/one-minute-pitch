86e601cbaeb58ccc44ea27b7a2f0260c5cddbfc6
peter kuria <you@example.com>
2020-01-13 11:57:26 -0500

    initialized flask extions

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,20 @@ from flas_login import loginManager
 bootstrap=Bootstrap()
 db=SQLAlchemy()
 login_manager=loginManager()
+
+
+def create_app(config_name):
+    app=Flask(__name__)
+    # create the app configurations.
+    app.config.from_object(config_options[config_name])
+
+    #initializing flask extentions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.session_protection='strong'
+    login_manager.login_view='auth.login'
+
+    #registering the blueprint
+    from .main import main as main_blueprint
+    
