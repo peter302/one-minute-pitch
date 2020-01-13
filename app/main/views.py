0f90fb980d41a78eac32556b217ca0724a60529e
peter kuria <you@example.com>
2020-01-13 14:26:02 -0500

    created new pitch function

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,25 @@ def index():
 
     title = 'Home- Welcome'
     return render_template('index.html', title = title, categories=category)
+
+
+
+#Route for adding a new pitch
+@main.route('/category/new-pitch/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_pitch(id):
+    ''' Function to check Pitches form and fetch data from the fields '''
+    form = PitchForm()
+    category = PitchCategory.query.filter_by(id=id).first()
+
+    if category is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        content = form.content.data
+        new_pitch= Pitch(content=content,category_id= category.id,user_id=current_user.id)
+        new_pitch.save_pitch()
+        return redirect(url_for('.category', id=category.id))
+
+    return render_template('new_pitch.html', pitch_form=form, category=category)
+    
