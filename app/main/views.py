50c53f7821e19e4ae04007369c4b18390a52c4bc
peter kuria <you@example.com>
2020-01-13 14:30:10 -0500

    added a new funtion new_pitch()

--- app/main/views.py
+++ app/main/views.py
@@ -46,3 +46,23 @@ def category(id):
 
     pitches=Pitch.get_pitches(id)
     return render_template('category.html', pitches=pitches, category=category)
+
+
+
+@main.route('/add/category', methods=['GET','POST'])
+@login_required
+def new_category():
+    '''
+    View new group route function that returns a page with a form to create a category
+    '''
+    form = CategoryForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = PitchCategory(name=name)
+        new_category.save_category()
+
+        return redirect(url_for('.index'))
+
+    title = 'New category'
+    return render_template('new_category.html', category_form = form,title=title)
