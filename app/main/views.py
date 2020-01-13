2b7451dd359adbefaeb23f959fe50cd6a3dfe787
peter kuria <you@example.com>
2020-01-13 14:37:58 -0500

    added a new function vote_count

--- app/main/views.py
+++ app/main/views.py
@@ -140,4 +140,16 @@ def downvote(id):
 
     new_vote = Votes(vote=int(2), user_id=current_user.id, pitches_id=pitch_id.id)
     new_vote.save_vote()
-    return redirect(url_for('.view_pitch', id=id))        
+    return redirect(url_for('.view_pitch', id=id))
+
+
+@main.route('/pitch/downvote/<int:id>')
+def vote_count(id):
+    '''
+    View function to return the total vote count per pitch
+    '''
+    votes = Votes.query.filter_by(user_id=user_id, line_id=line_id).all()
+
+    total_votes = votes.count()
+
+    return total_votes
