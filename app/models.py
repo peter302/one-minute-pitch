e8c575198c192d92f4a1f955d67da00c663ec33c
peter kuria <you@example.com>
2020-01-14 00:43:53 -0500

    added a new classs pitch

--- app/models.py
+++ app/models.py
@@ -67,3 +67,38 @@ class PitchCategory(db.Model):
     def get_categories(cls):
         categories = PitchCategory.query.all()
         return categories
+
+
+
+#pitches class
+class Pitch(db.Model):
+    """ List of pitches in each category """
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    content = db.Column(db.String)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship("Comments", backref="pitches", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="pitches", lazy = "dynamic")
+
+
+
+    def save_pitch(self):
+        ''' Save the pitches '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_pitches(cls):
+        Pitch.all_pitches.clear()
+
+    # display pitches
+
+    def get_pitches(id):
+        pitches = Pitch.query.filter_by(category_id=id).all()
+        return pitches
+
+
+        
