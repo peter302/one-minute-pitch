14d17bb58e1dabd2b13ce00573f7d99fa65908c1
peter kuria <you@example.com>
2020-01-14 00:28:46 -0500

    added a new class PitchCategory

--- app/models.py
+++ app/models.py
@@ -48,4 +48,22 @@ class User(UserMixin,db.Model):
         return f'User {self.username}'
 
 
-        
+#category model
+class PitchCategory(db.Model):
+
+    __tablename__ = 'categories'
+
+    # table columns
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+
+    # save pitches
+    def save_category(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        categories = PitchCategory.query.all()
+        return categories
