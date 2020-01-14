0bbbb715186a7dea5e4ea39fa601cc57e93c5e9a
peter kuria <you@example.com>
2020-01-14 00:44:42 -0500

    added a new class coments

--- app/models.py
+++ app/models.py
@@ -101,4 +101,30 @@ class Pitch(db.Model):
         return pitches
 
 
+# comments
+class Comments(db.Model):
+    '''User comment model for each pitch '''
+
+    __tablename__ = 'comments'
+
+    # add columns
+    id = db.Column(db. Integer, primary_key=True)
+    opinion = db.Column(db.String(255))
+    time_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+
+
+    def save_comment(self):
+        '''
+        Save the Comments/comments per pitch
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(self, id):
+        comment = Comments.query.order_by(
+            Comments.time_posted.desc()).filter_by(pitches_id=id).all()
+        return comment
         
