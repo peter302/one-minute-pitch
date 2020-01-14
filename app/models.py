4668d77ab7d03546639e3f2c09153a3b27012b35
peter kuria <you@example.com>
2020-01-14 00:21:50 -0500

    added a user model

--- app/models.py
+++ app/models.py
@@ -12,3 +12,21 @@ def load_user(user_id):
     a function to load users'
     '''
     return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    """ class modelling the users """
+
+    __tablename__='users'
+
+    #create the columns
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index =True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship("Pitch", backref="user", lazy = "dynamic")
+    comment = db.relationship("Comments", backref="user", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="user", lazy = "dynamic")    
